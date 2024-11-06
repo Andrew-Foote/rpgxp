66d3fd4b4b4c8cef76ad570307a2545a923afc3f
Andrew Foote <footeandrew1@gmail.com>
2024-11-06 02:41:27 +0000

    fix for tile layering

--- rpgxp/tile.py
+++ rpgxp/tile.py
@@ -1,7 +1,5 @@
-from dataclasses import dataclass
-from enum import Enum
 import io
-import math
+import itertools as it
 from pathlib import Path
 from typing import Iterator
 import numpy as np
@@ -12,32 +10,7 @@ from rpgxp.autotile import format as autotile
 from rpgxp.util import int_from_digits
 
 TILE_SIZE = 32
-
-@dataclass
-class Autotile:
-    id_: int
-
-@dataclass
-class RegularTile:
-    id_: int
-
-Tile = Autotile | RegularTile
-
-def itertiles(tileset_path: Path) -> Iterator[Image]:
-    with image.open(tileset_path).convert('RGBA') as tileset:
-        w = tileset.width
-        h = tileset.height
-
-        # # first row - autotiles
-        # for x in range(0, w, TILE_SIDE_LENGTH):
-        #     for _ in range(48):
-        #         yield tileset.crop((
-        #             x, 0, x + TILE_SIDE_LENGTH, TILE_SIDE_LENGTH
-        #         ))
-
-        for y in range(0, h, TILE_SIZE):
-            for x in range(0, w, TILE_SIZE):
-                yield tileset.crop((x, y, x + TILE_SIZE, y + TILE_SIZE))
+IMAGE_MODE = 'RGBA'
 
 MAP_QUERY = '''SELECT
     t.id, tm.name || tm.extension, tm.source,
@@ -48,13 +21,47 @@ JOIN material_best_file tm on tm.type = 'Graphics' and tm.subtype = 'Tilesets'
     and tm.name = t.tileset_name
 WHERE m.id = ?'''
 
-AUTOTILE_QUERY = '''SELECT am.name || am.extension, am.source
+AUTOTILE_QUERY = '''SELECT am.name || am.extension, am.source, a."index"
 FROM tileset_autotile a
 JOIN material_best_file am on am.type = 'Graphics' and am.subtype = 'Autotiles'
     and am.name = a.autotile_name
 WHERE a.tileset_id = ?
 ORDER BY a."index"
 '''
+
+def itertiles(tileset_path: Path) -> Iterator[Image]:
+    with image.open(tileset_path).convert(IMAGE_MODE) as tileset:
+        w = tileset.width
+        h = tileset.height
+
+        for y in range(0, h, TILE_SIZE):
+            for x in range(0, w, TILE_SIZE):
+                yield tileset.crop((x, y, x + TILE_SIZE, y + TILE_SIZE))
+
+def is_autotile(tile_id: int) -> bool:
+    return tile_id < 384
+
+def get_configuration(array: np.ndarray, point: tuple[int, int, int]) -> int:
+    # I'm assuming the autotile configuration only depends on the adjacent
+    # tiles in the same layer...
+    point_array = np.array(point)
+
+    offsets = [np.array((*offset, 0)) for offset in (
+        (-1, -1), (0, -1), (1, -1),
+        (-1,  0),          (1,  0),
+        (-1,  1), (0,  1), (1,  1)
+    )]
+
+    adjacents = [point_array + offset for offset in offsets]
+    shape_array = np.array(array.shape)
+
+    return int_from_digits([
+        (
+            not np.all((0 <= adj) & (adj < shape_array))
+            or is_autotile(array[tuple(adj)])
+        )
+        for adj in adjacents
+    ], 2)
 
 def map_image(map_id: int) -> Image:
     dbh = db.connect()
@@ -83,57 +90,41 @@ def map_image(map_id: int) -> Image:
     tileset_path = tileset_root / 'Graphics' / 'Tilesets' / tileset_name
     regular_tiles = list(itertiles(tileset_path))
     assert regular_tiles
-    mode = regular_tiles[0].mode
-    result = image.new(mode, (width * TILE_SIZE, height * TILE_SIZE))
-
-    for z in (0,):#range(depth):
-        for x in range(width):
-            for y in range(height):
-                tile_id = data_array[x, y, z]
-
-                if tile_id < 48:
-                    pass
-                elif tile_id < 384:
-                    autotile_cells.add((x, y, z))
-                else:
-                    tile = regular_tiles[tile_id - 384]
-                    result.paste(tile, (x * TILE_SIZE, y * TILE_SIZE), tile)
-
-    # autotiles
 
     autotile_rows = dbh.execute(AUTOTILE_QUERY, (tileset_id,)).fetchall()
     autotile_count = len(autotile_rows)
-    assert autotile_count == 7, autotile_count
+    assert autotile_count <= 7, autotile_count
 
-    autotile_images: list[Image] = []
+    autotile_images: list[Image | None] = [None for _ in range(8)]
 
-    for autotile_name, autotile_source in autotile_rows:
+    for autotile_name, autotile_source, index in autotile_rows:
         assert isinstance(autotile_name, str)
         assert isinstance(autotile_source, str)
+        assert isinstance(index, int)
         autotile_root = material.root_for_source(autotile_source)
         autotile_path = autotile_root / 'Graphics' / 'Autotiles' / autotile_name
-        
-        autotile_images.append(image.open(autotile_path).convert('RGBA'))
+        autotile_images[index + 1] = image.open(autotile_path).convert(IMAGE_MODE)
 
-    for x0, y0, z0 in autotile_cells:
-        tile_id = data_array[x0, y0, z0]
+    result = image.new(IMAGE_MODE, (width * TILE_SIZE, height * TILE_SIZE))
 
-        configuration = int_from_digits([
-            (
-                not (0 <= x < width and 0 <= y < height and 0 <= z < depth)
-                or (x, y, z) in autotile_cells
-            )
-            for x, y, z in [
-                (x0 - 1, y0 - 1, z0), (x0, y0 - 1, z0), (x0 + 1, y0 - 1, z0),
-                (x0 - 1, y0, z0),                       (x0 + 1, y0, z0),
-                (x0 - 1, y0 + 1, z0), (x0, y0 + 1, z0), (x0 + 1, y0 + 1, z0)
-            ]
-        ], 2)
+    # note that it's important that we iterate over z in ascending order, so
+    # that we get the correct layering
+    for x, y, z in it.product(range(width), range(height), range(depth)):
+        tile_id = data_array[x, y, z]
 
-        autotile_index = tile_id // 48
-        autotile_image = autotile_images[autotile_index - 1]
-        tile = autotile.configure(autotile_image, configuration)
-        result.paste(tile, (x0 * TILE_SIZE, y0 * TILE_SIZE), tile)
+        if is_autotile(tile_id):
+            autotile_index = tile_id // 48
+            autotile_image = autotile_images[autotile_index]
+
+            if autotile_image is None:
+                continue
+
+            config = get_configuration(data_array, (x, y, z))
+            tile = autotile.configure(autotile_image, config)
+        else:
+            tile = regular_tiles[tile_id - 384]
+
+        result.paste(tile, (x * TILE_SIZE, y * TILE_SIZE), tile)
             
     return result
 
